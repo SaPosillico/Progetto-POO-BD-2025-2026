1597a4a8e46b2b292e3f7e6734a38551ceb94e58
RossiAnto <rossiantonio0506@gmail.com>
2026-05-23 19:31:16 +0200

    login GUI Antonio Rossi
    23/05
    Problemi da risolvere della bozza:
    - I pulsanti della finestra di login non si restringono più correttamente quando in finestra piccola come le interfacce di film e proiezioni
    
    - Problemi con l'allineamento verticale, probabilmente dovuto al Jpanel dei pulsanti, che non riesco a risolvere

--- src/main/java/gui/PaginaLogInGUI.java
+++ src/main/java/gui/PaginaLogInGUI.java
@@ -3,6 +3,7 @@ package gui;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import controller.*;
 
 public class PaginaLogInGUI {
@@ -11,6 +12,10 @@ public class PaginaLogInGUI {
     private JButton filmButton;
     private JButton proiezioniButton;
     private JButton logInButton;
+    private JTextField emailTextField;
+    private JTextField passwordTextField;
+    private JLabel benvenutoLabel;
+    private JButton invioButton;
     private JFrame frameLogIn;
 
     public JFrame getFrame() {
@@ -23,6 +28,59 @@ public class PaginaLogInGUI {
         frameLogIn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameLogIn.pack();
         frameLogIn.setVisible(true);
+        frameLogIn.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        benvenutoLabel.setText("Benvenuto! Inserisci qui i dati del tuo account.");
+
+        //AR: non riuscivo a centrare la label
+        //benvenutoLabel.setPreferredSize(new java.awt.Dimension(2000, 100));
+
+        benvenutoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        benvenutoLabel.setVerticalAlignment(SwingConstants.CENTER);
+
+
+        emailTextField.setText("Email");
+
+        emailTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            @Override
+            public void focusGained(java.awt.event.FocusEvent e) {
+                if (emailTextField.getText().equals("Email")) {
+                    emailTextField.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(java.awt.event.FocusEvent e) {
+                if (emailTextField.getText().isEmpty()) {
+                    emailTextField.setText("Email");
+                }
+            }
+        });
+
+
+        passwordTextField.setText("Password");
+
+        passwordTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            @Override
+            public void focusGained(java.awt.event.FocusEvent e) {
+                if (passwordTextField.getText().equals("Password")) {
+                    passwordTextField.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(java.awt.event.FocusEvent e) {
+                if (passwordTextField.getText().isEmpty()) {
+                    passwordTextField.setText("Password");
+                }
+            }
+        });
+
+
+        invioButton.setText("Invio");
+        invioButton.setPreferredSize(new java.awt.Dimension(200, 50));
+
+
         homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
